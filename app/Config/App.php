7b34552178e52ef2e3703c272e6e797c3eb82081
zdeiby <deibygj@hotmail.com>
2023-06-19 02:58:44 +0200

    actualizacion

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/cod4/';
+    public string $baseURL = 'https://comercial.castelancarpinteyro.com';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
